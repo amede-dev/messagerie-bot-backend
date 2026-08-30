ebbeff1aef49dc759693bf8cfcb586d00a49f6c4
amede-dev <rakotonandrasanaamede5@gmail.com>
2026-08-30 10:12:23 +0300

    Update BotResponseDto.java

--- src/main/java/mg/eni/reseauuniversitaire/messageriebot/dto/BotResponseDto.java
+++ src/main/java/mg/eni/reseauuniversitaire/messageriebot/dto/BotResponseDto.java
@@ -2,8 +2,7 @@ package mg.eni.reseauuniversitaire.messageriebot.dto;
 
 import java.util.List;
 
-/// Correspond exactement a BotResponseModel.fromJson cote Flutter :
-/// texte / suggestions / escaladeHumaine.
+// Correspond exactement a BotResponseModel.fromJson cote Flutter : texte / suggestions / escaladeHumaine.
 public record BotResponseDto(
         String texte,
         List<String> suggestions,
