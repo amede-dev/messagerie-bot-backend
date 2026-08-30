13cc0d1e7a9e5fe9b8540a573c3c8ce78a8cb219
amede-dev <rakotonandrasanaamede5@gmail.com>
2026-08-30 10:14:41 +0300

    Update BotMessageRequestDto.java

--- src/main/java/mg/eni/reseauuniversitaire/messageriebot/dto/BotMessageRequestDto.java
+++ src/main/java/mg/eni/reseauuniversitaire/messageriebot/dto/BotMessageRequestDto.java
@@ -2,7 +2,5 @@ package mg.eni.reseauuniversitaire.messageriebot.dto;
 
 import jakarta.validation.constraints.NotBlank;
 
-import java.util.List;
-
 public record BotMessageRequestDto(@NotBlank String texte) {
 }
